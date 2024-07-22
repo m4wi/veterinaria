4306cf92b523460e83d7b848f55c343392d777e6
m4wi <mamanisandro951@gmail.com>
2024-07-22 17:33:22 -0500

    any changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import controllers from './controllers/auth.controller.js'
-import controllers2 from './controllers/sabana.controller.js'
+// import controllers2 from './controllers/sabana.controller.js'
 
 const app = express()
 
@@ -14,7 +14,7 @@ app.get("/animal/status", controllers.validAnimal)
 
 app. get("/animal/:arete",(req,res) => {res.send("not implemented")})
 
-app.get("/sabana/:type", controllers2.sabanas)
+// app.get("/sabana/:type", controllers2.sabanas)
 
 app.listen(3000, () => {
   console.log(`Example app listening on port 3000`)
