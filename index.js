56dbc7c0508366458ee3fdd0857532ef1d4fede6
m4wi <mamanisandro951@gmail.com>
2024-07-22 18:26:04 -0500

    any changes

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 import express from 'express'
-import controllers from './src/controllers/auth.controller.js'
-// import controllers2 from './controllers/sabana.controller.js'
 
 const app = express()
 
-app.use(express.json())
+// app.use(express.json())
 
 app.get("/",(req, res) => {
   res.send("Welcome to the API")
 })
 
-app.get("/animal/status", controllers.validAnimal)
+// app.get("/animal/status", controllers.validAnimal)
 
-app. get("/animal/:arete",(req,res) => {res.send("not implemented")})
+// app. get("/animal/:arete",(req,res) => {res.send("not implemented")})
 
 // app.get("/sabana/:type", controllers2.sabanas)
 
